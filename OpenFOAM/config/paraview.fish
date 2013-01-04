983be30b5ca9f84038dbffd722101ec3318755c9
Laurence R. McGlashan <laurence.mcglashan@gmail.com>
2013-01-04 23:14:53 +0000

    ENH: Updates to environment variables

--- OpenFOAM/config/paraview.fish
+++ OpenFOAM/config/paraview.fish
@@ -71,7 +71,7 @@ switch "$ParaView_VERSION"
 end
 
 set -l paraviewInstDir $WM_THIRD_PARTY_DIR/ParaView-$ParaView_VERSION
-set -x ParaView_DIR /opt/paraviewopenfoam3120
+set -x ParaView_DIR $WM_THIRD_PARTY_DIR/platforms/$WM_ARCH$WM_COMPILER/paraview-$ParaView_VERSION
 
 if test -r $ParaView_DIR; or test -r $paraviewInstDir
     prependPath PATH $ParaView_DIR/bin
@@ -87,5 +87,6 @@ if test -r $ParaView_DIR; or test -r $paraviewInstDir
 else
     set -e PV_PLUGIN_PATH
 end
+
 
 # -----------------------------------------------------------------------------
